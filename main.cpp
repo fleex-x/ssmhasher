5bcd6050a61d62eacea2ed59644113762973de42
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2022-03-19 13:24:52 +0300

    Add -O2

--- main.cpp
+++ main.cpp
@@ -8,9 +8,20 @@
 
 #include <iostream>
 
+void raw_access_wrapper(std::byte * in, std::size_t key_len, std::byte * out, std::size_t out_len) {
+    assert(key_len <= out_len);
+    // memcpy((void*)out, (const void *)in, (int)key_len);
+     for (int i = 0; i < key_len; ++i) {
+        volatile std::byte x = in[i];
+    }
+}
+
 void memcpy_wrapper(std::byte * in, std::size_t key_len, std::byte * out, std::size_t out_len) {
     assert(key_len <= out_len);
     memcpy((void*)out, (const void *)in, (int)key_len);
+    //  for (int i = 0; i < key_len; ++i) {
+        // volatile std::byte x = in[i];
+    // }
 }
 
 int main() {
@@ -20,16 +31,25 @@ int main() {
 
     TestGen tg;
     auto b = std::chrono::steady_clock::now();
-    auto snds = SpeedTest(xxhash32, 5, tg, 1'000'000, 4);
+    auto snds = SpeedTest(xxhash64, 5, tg, 1'000'000, 4);
 
-    std::cout << "Time taken(xxsh32), microsec.: " << snds << '\n';
+    std::cout << "Time taken(xxsh_64), microsec.: " << snds << '\n';
+
+    snds = SpeedTest(murMurHash3_128, 5, tg, 1'000'000, 8);
+
+    std::cout << "Time taken(mmh3_128), microsec.: " << snds << '\n';
 
     snds = SpeedTest(xxhash64, 5, tg, 1'000'000, 8);
 
-    std::cout << "Time taken(xxhash64), microsec.: " << snds << '\n';
+    std::cout << "Time taken(xxhash_64), microsec.: " << snds << '\n';
 
-    snds = SpeedTest(murMurHash2_64, 5, tg, 1'000'000, 8);
+    snds = SpeedTest(raw_access_wrapper, 5, tg, 1'000'000, 1'000'000);
 
-    std::cout << "Time taken(mmh2_64), microsec.: " << snds << '\n';
+    std::cout << "Time taken(just for by byte), microsec.: " << snds << '\n';
+
+    snds = SpeedTest(memcpy_wrapper, 5, tg, 1'000'000, 1'000'000);
+
+    std::cout << "Time taken(memcpy), microsec.: " << snds << '\n';
+    
 
 }
